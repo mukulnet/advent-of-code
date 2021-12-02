9f3c171e108c1b6c8f508742ffb0e74673d86000
Mukul Patil <pmukul@vmware.com>
2021-12-02 13:42:29 -0800

    Advent of Code Day 2 [C++]

---
+++ advent4.cpp
@@ -0,0 +1,66 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+using namespace std;
+ifstream infile("input2.txt");
+// ifstream infile("test.txt");
+
+struct position
+{
+    string direction;
+    unsigned int unit;
+};
+
+
+int main()
+{
+    unsigned int unit;
+    unsigned int count = 0;
+    vector<position> positions;
+    vector<unsigned int> sums;
+    unsigned int horizontal = 0;
+    unsigned int depth = 0;
+    unsigned int aim = 0;
+    string direction;
+
+    while (infile >> direction >> unit)
+    {
+        cout << direction << endl;
+        cout << unit << endl;
+        position temp_position {direction, unit};
+
+        positions.push_back(temp_position);
+    }
+
+    cout << "Size: " << positions.size() << endl;
+
+    for (int i = 0; i < positions.size(); i++)
+    {
+        if (positions[i].direction == "forward")
+        {
+            horizontal += positions[i].unit;
+            depth += (aim * positions[i].unit);
+        }
+        if (positions[i].direction == "up")
+        {
+            aim -= positions[i].unit;
+        }
+        if (positions[i].direction == "down")
+        {
+            aim += positions[i].unit;
+        }
+    }
+
+
+    //    for (int i = 1; i < sums.size(); i++)
+    //    {
+    //        if (sums[i] > sums[i - 1])
+    //        {
+    //            count++;
+    //        }
+    //    }
+
+    cout << "Results: " << horizontal << "  and " << depth << endl;
+    cout << "Output: " << horizontal * depth << endl;
+    return 0;
+}
